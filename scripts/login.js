841ee06208151889f471fe18549d142caa34da4d
Ruben Sepulveda <rsepulveda@planok.com>
2024-12-21 08:11:42 -0300

    version estable antes de pruebas con express y mysql

--- scripts/login.js
+++ scripts/login.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 require('dotenv').config();
 
 async function login() {
-    const browser = await puppeteer.launch({ headless: false }); // No headless para ver la ejecución
+    const browser = await puppeteer.launch({ headless: true }); // No headless para ver la ejecución
     const page = await browser.newPage();
 
     // Navega a la página de login
@@ -18,7 +18,7 @@ async function login() {
 
     // Espera a que la página de inicio se cargue
     await page.waitForNavigation();
-    console.log('Inició sesión correctamente');
+    console.log('\033[32;5m-LOGIN-\033[32m');
 
     // Retorna el navegador y la página para reutilizarlos en la prueba
     return { browser, page };
