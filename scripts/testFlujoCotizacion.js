34401ff323908ac788cedb1831606efcbbbe5f66
Ruben Sepulveda <rsepulveda@planok.com>
2025-01-08 10:52:52 -0300

    se ordenan acciones con bloques try-catch

--- scripts/testFlujoCotizacion.js
+++ scripts/testFlujoCotizacion.js
@@ -50,15 +50,25 @@ module.exports = async function testFormSubmission(page) {
 
     /* CLICK EN BOTON FICHA DE CLIENTE */
     console.log("- ABRIENDO FICHA CLIENTE...-");
-    await page.waitForSelector('a.btn.btn-xs.btn-default[title="Ficha Cliente"]');
-    await page.click('a.btn.btn-xs.btn-default[title="Ficha Cliente"]');
-    console.log("- Clic en el enlace 'Ficha Cliente' realizado. -");
+    try {
+        await page.waitForSelector('a.btn.btn-xs.btn-default[title="Ficha Cliente"]');
+        await page.click('a.btn.btn-xs.btn-default[title="Ficha Cliente"]');
+        console.log("- Clic en el enlace 'Ficha Cliente' realizado. -");
+    } catch (err) {
+        console.log("\033[32m- ERROR EN CLICK FICHA CLIENTE. -\033[0m");
+    }
+
 
     /* CLICK EN EL BOTON COTIZAR EN SALA */
     console.log("- Simulando clic en el elemento 'Cotización en Sala'... -");
-    await page.waitForSelector('#coti_sala');
-    await page.click('#coti_sala');
-    console.log("- Clic en el elemento 'Cotización en Sala' realizado. -");
+    try {
+        await page.waitForSelector('#coti_sala');
+        await page.click('#coti_sala');
+        console.log("- Click en el elemento 'Cotización en Sala' realizado. -");
+    } catch (err){
+        console.log("\033[32m- ERROR click en el elemento 'Cotización en Sala'... -\033[0m");
+    }
+    
 
     /* CLICK EN EL PRIMER PRODUCTO PRINCIPAL */
     /* SELECCIONAR EL PRIMER ELEMENTO DEL SELECTOR SIN CONOCER SU VALOR */
@@ -79,66 +89,105 @@ module.exports = async function testFormSubmission(page) {
 
     /* SIMULAR CLICK EN EL BOTÓN */
     console.log("- Simulando el clic en el botón 'Agregar Producto'... -");
-    await page.waitForSelector('#sel_boton_agregar');
-    await page.click('#sel_boton_agregar');
-    console.log("- Clic en el botón realizado exitosamente. -");
+    try {
+        await page.waitForSelector('#sel_boton_agregar');
+        await page.click('#sel_boton_agregar');
+        console.log("- Clic en el botón realizado exitosamente. -");
+    }catch(err) {
+        console.log("\033[32m- ERROR el clic en el botón 'Agregar Producto'... -\033[0m");
+    }
+
     
 
     /* SIMULAR CLICK EN EL BOTÓN 'Guardar' */
     console.log("- Simulando el clic en el botón 'Guardar'... -");
-    await page.waitForSelector('#boton_guardar');
-    await page.click('#boton_guardar');
-    console.log("- Clic en el botón 'Guardar' realizado exitosamente. -");
+    try {
+        await page.waitForSelector('#boton_guardar');
+        await page.click('#boton_guardar');
+        console.log("- Clic en el botón 'Guardar' realizado exitosamente. -");
+    }catch(err){
+        console.log("\033[32m- ERROR EN clic en el botón 'Guardar'... -\033[0m");
+    }
+
 
     /* SIMULAR EVALUACION */
     console.log("- Simulando el clic en el botón 'Debe evaluar cliente'... -");
-    await page.waitForSelector('#btn_evaluar');
-    await page.click('#btn_evaluar');
-    console.log("- Clic en el botón 'Debe evaluar cliente' realizado exitosamente. -");
+    try {
+        await page.waitForSelector('#btn_evaluar');
+        await page.click('#btn_evaluar');
+        console.log("- Clic en el botón 'Debe evaluar cliente' realizado exitosamente. -");
+    }catch(err){
+        console.log("- ERROR: Simulando el click en el botón 'Debe evaluar cliente'... -");
+    }
+
 
     /* SIMULAR LLENADO MODAL EVALUACION CLIENTE */
     /* SELECCIONA EXPECTATIVA */
     console.log("- SIMULANDO MODAL 'Evaluacion Cliente'... -");
-    await page.waitForSelector('#id_expectativa');
-    await page.evaluate(() => {
-        const selectExpectativa = document.querySelector('#id_expectativa');
-        if (selectExpectativa && selectExpectativa.options.length > 1) {
-            selectExpectativa.selectedIndex = 1; // Cambia la selección al segundo elemento
-            selectExpectativa.dispatchEvent(new Event('change')); // Dispara el evento 'change' para reflejar el cambio
-        }
-    });
-    console.log("- La primera EXPECTATIVA fue seleccionada correctamente. -");
+    try {
+        await page.waitForSelector('#id_expectativa');
+        await page.evaluate(() => {
+            const selectExpectativa = document.querySelector('#id_expectativa');
+            if (selectExpectativa && selectExpectativa.options.length > 1) {
+                selectExpectativa.selectedIndex = 1; // Cambia la selección al segundo elemento
+                selectExpectativa.dispatchEvent(new Event('change')); // Dispara el evento 'change' para reflejar el cambio
+            }
+        });
+        console.log("- La primera EXPECTATIVA fue seleccionada correctamente. -");
+    }catch(err){
+        console.log("- ERROR: SIMULANDO MODAL 'Evaluacion Cliente'... -");
+    }
+
 
     /* SELECCIONA RAZON DE COMPRA */
-    await page.waitForSelector('#id_razon_compra');
-    await page.evaluate(() => {
-        const selectRazonCompra = document.querySelector('#id_razon_compra');
-        if (selectRazonCompra && selectRazonCompra.options.length > 1) {
-            selectRazonCompra.selectedIndex = 1; // Cambia la selección al segundo elemento
-            selectRazonCompra.dispatchEvent(new Event('change')); // Dispara el evento 'change' para reflejar el cambio
-        }
-    });
-    console.log("- La primera RAZON DE COMPRA fue seleccionada correctamente. -");
+    console.log("- simulando selector 'Razón de compra'... -");
+    try {
+        await page.waitForSelector('#id_razon_compra');
+        await page.evaluate(() => {
+            const selectRazonCompra = document.querySelector('#id_razon_compra');
+            if (selectRazonCompra && selectRazonCompra.options.length > 1) {
+                selectRazonCompra.selectedIndex = 1; // Cambia la selección al segundo elemento
+                selectRazonCompra.dispatchEvent(new Event('change')); // Dispara el evento 'change' para reflejar el cambio
+            }
+        });
+        console.log("- La primera RAZON DE COMPRA fue seleccionada correctamente. -");
+    }catch(err){
+        console.log("- ERROR: simulando selector 'Razón de compra'... -");
+    }
 
-
+    /* llenado textarea evaluacion*/
     console.log("- Llenando el campo de texto área con el contenido 'SISTEMA DE TESTING AUTOMATIZADO'... -");
-    await page.waitForSelector('#comentario');
-    const currentDateTime = new Date().toLocaleString();
-    const textoComentario = `SISTEMA DE TESTING AUTOMATIZADO - ${currentDateTime}`;
-    await page.type('#comentario', textoComentario);
-    console.log("- El campo de texto área fue llenado correctamente. -");
+    try{
+        await page.waitForSelector('#comentario');
+        const currentDateTime = new Date().toLocaleString();
+        const textoComentario = `SISTEMA DE TESTING AUTOMATIZADO - ${currentDateTime}`;
+        await page.type('#comentario', textoComentario);
+        console.log("- El campo de texto área fue llenado correctamente. -");
+    }catch(err){
+        console.log("- ERROR: Llenando el campo de texto área ... -");
+    }
+    
+    /* click guardar evaluacion */
+    console.log("- Click en el botón 'Guardar Evaluación' -");
+    try {
+        await page.click('#btn_guardar_evaluacion');
+        console.log("- Click en el botón 'Guardar Evaluación' realizado exitosamente. -");
+    }catch(err){
+        console.log("- ERROR: Click en el botón 'Guardar Evaluación'-");
+    }
 
-    await page.click('#btn_guardar_evaluacion');
-    console.log("- Clic en el botón 'Guardar Evaluación' realizado exitosamente. -");
     
     /* CLICK EN ALERT */
     console.log("- Esperando alerta... -");
-    page.once('dialog', async dialog => {
-        console.log(`- Alerta mostrada: '${dialog.message()}' -`);
-        await dialog.accept(); // Aceptar la alerta
-        console.log("- Alerta aceptada. -");
-    });
-    console.log("- Acción realizada, alerta manejada correctamente. -");
-
+    try {
+        page.once('dialog', async dialog => {
+            console.log(`- Alerta mostrada: '${dialog.message()}' -`);
+            await dialog.accept(); // Aceptar la alerta
+            console.log("- Alerta aceptada. -");
+        });
+        console.log("- Acción realizada, alerta manejada correctamente. -");
+    }catch(err){
+        console.log("- ERROR: Esperando alerta... -");
+    }
 
 };
